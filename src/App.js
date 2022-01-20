9904f827648b37f0d9a0bf0570415e6821e80a67
Pamela Castaneda <pamelacastanedav@gmail.com>
2022-01-19 18:44:03 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  //set as empy arry
+  const [pokemons, setPokemons] = useState([]);
+  const fetchPoke = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((pokemons) => pokemons.json())
+
+      //will return the json results
+      .then((jsonRes) => {
+        setPokemons(jsonRes.results);
+        // console.log(jsonRes.results);
+      })
+
+      .catch((someErr) => console.log(someErr));
+  };
+  //AXIOS requires to access trhu data
+  const axiosPokemons = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((res) => {
+        console.log(res.data.results);
+        setPokemons(res.data.results);
+      })
+
+      //axio wraps res into data.obj
+      .catch((err) => console.log(err));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokemons</h1>
+      <button onClick={fetchPoke}>Get Pokemons</button>
+      {/* this is to check if is working */}
+      {/* {JSON.stringify(pokemons)} */}
+
+      <button onClick={axiosPokemons}>Axio Pokemons</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Name:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemons.map((pokemon, idx) => {
+            return (
+              <tr key={idx}>
+                <td> {pokemon.name}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
